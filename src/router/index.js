029b3e6719d73d8fea2ddcd8bbb8143b468ee807
itzzbas <74045534+itzzbas@users.noreply.github.com>
2022-05-24 14:06:21 +0200

    Overview completed, en een brandview gemaakt waar alle producten ingezien kunnen worden.

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
   {
     path: '/create',
     name: 'create',
     component: () => import('../views/CreateView.vue')
   },
     {
-        path: '/overview',
+        path: '/',
         name: 'overview',
         component: () => import('../views/OverviewView.vue')
+    },
+    {
+        path: '/brand/:brandid',
+        name: 'brand',
+        component: () => import('../views/BrandView.vue')
     }
 ]
 
