5f256b04f6185e5ee3ef8ad0091512bf44798ea8
Jenson <431433@student.fontys.nl>
2022-06-07 12:10:04 +0200

    Auth0 login en logout first steps

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
@@ -19,6 +24,19 @@ Vue.use(IconsPlugin)
 Vue.config.productionTip = false;
 
 Vue.prototype.$http = axios;
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+    domain,
+    clientId,
+    onRedirectCallback: appState => {
+        router.push(
+            appState && appState.targetUrl
+                ? appState.targetUrl
+                : window.location.pathname
+        );
+    }
+});
 
 new Vue({
   router,
